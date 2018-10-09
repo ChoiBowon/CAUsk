15703f27e296f92f2dfac77b27833fbbb2cbc962
ChoiBowon <bowon1202@gmail.com>
2018-10-09 22:03:06 +0900

    logout change

--- logoutprocess.php
+++ logoutprocess.php
@@ -2,6 +2,7 @@
     session_start();
     session_destroy();
 
- echo ("<script>alert('로그아웃 되었습니다.');history.go(-1);</script>");
+ echo ("<script>alert('로그아웃 되었습니다.');history.go(-2);</script>");
+ exit;
      header("location: Main.php");
 ?>
