dcf09b89fd582d802dad11c4fe8e49894c403565
ChoiBowon <bowon1202@gmail.com>
2018-10-09 22:17:27 +0900

    store DB 연동

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
   $conn = mysqli_connect("ec2-13-125-242-247.ap-northeast-2.compute.amazonaws.com", "root", "bowon5263");
-  mysqli_select_db($conn, "testbowon");
+  mysqli_select_db($conn, "causk");
  ?>
