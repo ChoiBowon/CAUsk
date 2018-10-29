0afc4a79500a124f098fdedbfb780620d52946b7
ChoiBowon <bowon1202@gmail.com>
2018-10-30 01:58:34 +0900

    menu add

--- insertmenuprocess.php
+++ insertmenuprocess.php
@@ -2,7 +2,7 @@
 require_once("connect.php");
 session_start();
 
- $Category = $_POST['CategoryDirect.value'];
+ $Category = $_POST['CategoryDirect'];
  $MenuName = $_POST['MenuName'];
  $Size = $_POST['SizeDirect'];
  $HotIce = $_POST['hotDirect'];
@@ -13,7 +13,7 @@ console.log($_POST['CategoryDirect.value']);
 console.log($_POST['SizeDirect']);
 
  // $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['hotDirect']."','".$_POST['Price']."')";
- $sql="INSERT INTO menu (storeSerial,category,menuName,size,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect.value']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['Price']."')";
+ $sql="INSERT INTO menu (storeSerial,category,menuName,size,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['Price']."')";
 
    $result=mysqli_query($conn, $sql);
 
