cee59c05d97055aa65b518396a6cb8bb967347e3
ChoiBowon <bowon1202@gmail.com>
2018-10-30 02:29:42 +0900

    menu add

--- insertmenuprocess.php
+++ insertmenuprocess.php
@@ -4,22 +4,26 @@ session_start();
 
  $Category = $_POST['CategoryDirect'];
  $MenuName = $_POST['MenuName'];
- $Size = $_POST['SizeDirect'];
- $HotIce = $_POST['hotDirect'];
+ $Size = $_POST['size_selbox'];
+ $HotIce = $_POST['hot_selbox'];
  $Price = $_POST['Price'];
 
 echo('<pre>'); print_r($_POST); echo('</pre>');
 
+if ($HotIce == 'hot'){
+  $HotIce = 0;
+}
+echo('$HotIce');
 
  // $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['hotDirect']."','".$_POST['Price']."')";
- $sql="INSERT INTO menu (storeSerial,category,menuName,size,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['category_selbox']."','".$_POST['MenuName']."','".$_POST['size_selbox']."','".$_POST['Price']."')";
+ $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['category_selbox']."','".$_POST['MenuName']."','".$_POST['size_selbox']."','"$HotIce"','".$_POST['Price']."')";
 
    $result=mysqli_query($conn, $sql);
 
   //  echo ("<script>alert('메뉴가 추가되었습니다.');history.go(-1);</script>");
   // exit;
-
-
+  //
+  //
   // header("Location:Main2.php");
 
 
