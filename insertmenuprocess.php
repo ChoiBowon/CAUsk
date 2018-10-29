560845c3f7660ed5e360e27ff0cf0b43994e9afc
ChoiBowon <bowon1202@gmail.com>
2018-10-30 02:16:44 +0900

    menu add

--- insertmenuprocess.php
+++ insertmenuprocess.php
@@ -12,12 +12,12 @@ echo('<pre>'); print_r($_POST); echo('</pre>');
 
 
  // $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['hotDirect']."','".$_POST['Price']."')";
- $sql="INSERT INTO menu (storeSerial,category,menuName,size,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['Price']."')";
+ $sql="INSERT INTO menu (storeSerial,category,menuName,size,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['category_selbox']."','".$_POST['MenuName']."','".$_POST['size_selbox']."','".$_POST['Price']."')";
 
    $result=mysqli_query($conn, $sql);
 
   //  echo ("<script>alert('메뉴가 추가되었습니다.');history.go(-1);</script>");
-  exit;
+  // exit;
 
 
   // header("Location:Main2.php");
