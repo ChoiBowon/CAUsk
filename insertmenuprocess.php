659a11bff5937ac2ed0ce9d4c4fb32d125252827
ChoiBowon <bowon1202@gmail.com>
2018-10-30 03:00:13 +0900

    menu add

--- insertmenuprocess.php
+++ insertmenuprocess.php
@@ -16,11 +16,15 @@ if ($HotIce == 'hot'){
   $HotIce = 0;
 }elseif ($HotIce == 'ice') {
   $HotIce = 1;
+}elseif($HotIce == 'hot and ice'){
+  $HotIce = 2;
+}else{
+  $HotIce = null;
 }
 echo($HotIce);
 
  // $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['CategoryDirect']."','".$_POST['MenuName']."','".$_POST['SizeDirect']."','".$_POST['hotDirect']."','".$_POST['Price']."')";
- $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['category_selbox']."','".$_POST['MenuName']."','".$_POST['size_selbox']."','".$_POST['Price']."')";
+ $sql="INSERT INTO menu (storeSerial,category,menuName,size,hotIce,price) VALUES('".$_SESSION['storeSerial']."','".$_POST['category_selbox']."','".$_POST['MenuName']."','".$_POST['size_selbox']."','$HotIce','".$_POST['Price']."')";
 
    $result=mysqli_query($conn, $sql);
 
