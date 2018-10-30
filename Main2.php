7eb37527dbf8cfad31822a2f8fe82d853bc6b764
ChoiBowon <bowon1202@gmail.com>
2018-10-31 06:48:50 +0900

    menu add

--- Main2.php
+++ Main2.php
@@ -80,19 +80,20 @@ $sql = mysqli_query($conn, "SELECT DISTINCT category FROM menu WHERE storeSerial
 
      while($row = mysqli_fetch_assoc($result)){
        for ($j=0; $j<$numrow; $j++){
-         if ($category_list[$j] == $row['category']){
+         echo "<h2 class='section-title wow fadeInUp' data-wow-duration='1000ms' data-wow-delay='300ms' style='font-family: 'Do Hyeon', sans-serif;'>".$category_list[$j]."</h1>";
+         echo "<table class='table table-bordered'  style='font-family: 'Do Hyeon', sans-serif;'>
+             <thead>
+              <tr>
+                  <th scope='col'>Menu</th>
+                  <th scope='col'>Size</th>
+                  <th scope='col'>Hot/Ice</th>
+                  <th scope='col'>Price</th>
+              </tr>
+              </thead>";
+         if ($row['category'] == $category_list[$j]){
           //  echo "<h2 class='section-title wow fadeInUp' data-wow-duration='1000ms' data-wow-delay='300ms' style='font-family: 'Do Hyeon', sans-serif;'>".$category_list[$j]."</h1>";
-          echo "<h2 class='section-title wow fadeInUp' data-wow-duration='1000ms' data-wow-delay='300ms' style='font-family: 'Do Hyeon', sans-serif;'>".$category_list[$j]."</h1>";
 
-          echo "<table class='table table-bordered'  style='font-family: 'Do Hyeon', sans-serif;'>
-              <thead>
-               <tr>
-                   <th scope='col'>Menu</th>
-                   <th scope='col'>Size</th>
-                   <th scope='col'>Hot/Ice</th>
-                   <th scope='col'>Price</th>
-               </tr>
-               </thead>";
+
          }
        }
 
