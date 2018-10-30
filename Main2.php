f64bfc6e42aaaf20d75e6a963bf122197dbc7764
ChoiBowon <bowon1202@gmail.com>
2018-10-31 04:17:58 +0900

    menu add

--- Main2.php
+++ Main2.php
@@ -3,7 +3,8 @@ require_once("connect.php");
 $result = mysqli_query($conn,'SELECT*FROM menu WHERE storeSerial = '.$_SESSION['storeSerial'].'');
 session_start();
 // $sql = mysqli_query($conn, 'SELECT DISTINCT category FROM menu WHERE storeSerial = '.$_SESSION['storeSerial'].'');
-$sql = mysql_query("SELECT DISTINCT category FROM menu WHERE storeSerial = '.$_SESSION['storeSerial'].'");
+$sql = "SELECT DISTINCT category FROM menu WHERE storeSerial = '.$_SESSION['storeSerial'].'";
+$result1 = mysqli_query($conn, $sql);
 
 // $sql = "SELECT DISTINCT category FROM menu WHERE storeSerial = '.$_SESSION['storeSerial'].'";
 // $sql2 = mysqli_query($sql);
@@ -64,7 +65,7 @@ $sql = mysql_query("SELECT DISTINCT category FROM menu WHERE storeSerial = '.$_S
     <?php
     // $category_list = mysqli_fetch_assoc($sql);
     $category_list = [0];
-    $numrow = mysqli_num_rows($sql);
+    $numrow = mysqli_num_rows($result1);
     echo $numrow;
     var_dump($numrow);
     console.log($sql);
