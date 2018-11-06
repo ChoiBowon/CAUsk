51233269c5cc0c563d07c45f8c01bafcdc722b07
ChoiBowon <bowon1202@gmail.com>
2018-11-07 04:01:00 +0900

    revision

--- Main2.php
+++ Main2.php
@@ -230,17 +230,17 @@ $sql = mysqli_query($conn, "SELECT DISTINCT category FROM menu WHERE storeSerial
                 echo "</tr>";
                 echo "</tbody>";
 
-                // echo "<div id='menuModal' class='dialog' tabindex='-1' role='dialog' aria-hidden='true'>";
-                // echo "<div class='col-lg-8 col-md-8 col-sm-8 col-xs-8 dialog-content' style='background: #fbfbfb; box-shadow: 5px 5px grey'>";
-                // echo "<div class='dialog-header'>";
-                // echo "<h2 class='dialog-title' id='ModalLongTitle'>메뉴를 수정해주세요.</h2>";
-                // echo "</div>";
-                // echo "<div class='dialog-body'>";
-                // echo "</div>";
-                // echo "<div class='dialog-footer'>";
-                // echo "</div>";
-                // echo "</div>";
-                // echo "</div>";
+                echo "<div id='menuModal' class='dialog' tabindex='-1' role='dialog' aria-hidden='true'>";
+                echo "<div class='col-lg-8 col-md-8 col-sm-8 col-xs-8 dialog-content' style='background: #fbfbfb; box-shadow: 5px 5px grey'>";
+                echo "<div class='dialog-header'>";
+                echo "<h2 class='dialog-title' id='ModalLongTitle'>메뉴를 수정해주세요.</h2>";
+                echo "</div>";
+                echo "<div class='dialog-body'>";
+                echo "</div>";
+                echo "<div class='dialog-footer'>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
          }else{
            break;
          }
@@ -252,7 +252,7 @@ $sql = mysqli_query($conn, "SELECT DISTINCT category FROM menu WHERE storeSerial
 
 
           ?>
-          <div id="menuModal" class="dialog" tabindex="-1" role="dialog" aria-hidden="true">
+          <!-- <div id="menuModal" class="dialog" tabindex="-1" role="dialog" aria-hidden="true">
           <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 dialog-content" style="background: #fbfbfb; box-shadow: 5px 5px grey">
               <div class="dialog-header">
                 <h2 class="dialog-title" id="ModalLongTitle">메뉴를 수정해주세요.</h2>
@@ -357,7 +357,7 @@ $sql = mysqli_query($conn, "SELECT DISTINCT category FROM menu WHERE storeSerial
               </div>
             </form>
           </div>
-      </div>
+      </div> -->
 
 
 
