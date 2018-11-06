3e763b63fee5979290f12293fee720d522107bfc
ChoiBowon <bowon1202@gmail.com>
2018-11-07 02:40:33 +0900

    revision

--- js/dialog.js
+++ js/dialog.js
@@ -63,7 +63,7 @@ function hot(){
 
 document.addEventListener("DOMContentLoaded", function() {
   showChart(false);
-  
+  showRevision(false);
   $("#myBtn").addEventListener("click", addMenuListener);
   $("#close").addEventListener("click", closeMenuListener);
   $("#submit").addEventListener("click", closeMenuListener);
