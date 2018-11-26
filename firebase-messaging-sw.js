0d07f030670d5f2d0f5c145d5943380524e8ce43
ChoiBowon <bowon1202@gmail.com>
2018-11-27 01:20:26 +0900

    Service Worker test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,20 @@
-if(typeof importScripts == "function") {
-// web worker scope
-importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-messaging.js');
-
-var config = {
-    apiKey: "AIzaSyA55pkgysAfPFIETtD-eMPn0ORPN4LVmec",
-    authDomain: "causk-16a1b.firebaseapp.com",
-    databaseURL: "https://causk-16a1b.firebaseio.com",
-    projectId: "causk-16a1b",
-    storageBucket: "causk-16a1b.appspot.com",
-    messagingSenderId: "843739767329"
-  };
-  firebase.initializeApp(config);
-
-const messaging = firebase.messaging();
-}
-else {
-// not web worker scope, assume window scope
-}
+// if(typeof importScripts == "function") {
+// // web worker scope
+// importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-messaging.js');
+//
+// var config = {
+//     apiKey: "AIzaSyA55pkgysAfPFIETtD-eMPn0ORPN4LVmec",
+//     authDomain: "causk-16a1b.firebaseapp.com",
+//     databaseURL: "https://causk-16a1b.firebaseio.com",
+//     projectId: "causk-16a1b",
+//     storageBucket: "causk-16a1b.appspot.com",
+//     messagingSenderId: "843739767329"
+//   };
+//   firebase.initializeApp(config);
+// 
+// const messaging = firebase.messaging();
+// }
+// else {
+// // not web worker scope, assume window scope
+// }
